caa681e26bef8f37538259112a2ef8df251520d7
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2019-05-30 16:15:21 +0700

    Create coinControl.cs

---
+++ coinControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 1, 0);
+
+
+    }
+}
